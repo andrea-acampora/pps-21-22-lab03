e0ec091be1a90f33dba084fc4348d88029463f19
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-14 23:32:15 +0100

    test: create test for stream functions

---
+++ src/test/scala/u03/StreamTest.scala
@@ -0,0 +1,23 @@
+package u03
+
+import org.junit.*
+import org.junit.Assert.*
+
+import u03.Streams.*
+import u03.Streams.Stream.*
+import u03.Lists.List.Cons
+import u03.Lists.List.Nil
+
+
+class StreamTest :
+
+  val stream: Stream[Int] = take(iterate(0)(_ + 1))(10)
+
+  @Test def testDrop(): Unit =
+    assertEquals( Stream.toList(cons(8, cons(9, empty()))), Stream.toList(drop(stream)(8)))
+
+  @Test def testConstant(): Unit =
+    assertEquals(Stream.toList(cons("x", cons("x", cons("x", empty())))), Stream.toList(take(constant("x"))(3)))
+
+  @Test def testFib(): Unit =
+    assertEquals(Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Cons(5, Nil())))))), toList(take(fib())(6)))
