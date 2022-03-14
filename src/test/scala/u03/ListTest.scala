e2df8584edb9af91274a3b4a801438a20120e756
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-14 10:32:16 +0100

    test: create test for drop function

--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -9,14 +9,17 @@ class ListTest:
 
   val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(0, sum(Nil()))
     assertEquals(60, sum(l))
 
-  @Test def testMap() =
+  @Test def testMap(): Unit =
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_ + 1))
     assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
 
-  @Test def testFilter() =
+  @Test def testFilter(): Unit =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test def testDrop(): Unit =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 1));
