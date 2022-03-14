b6a275e5d7ca00f6205a672f9c5b26f1d06041b1
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-14 11:30:39 +0100

    refactor: use flatMap function in map and filter

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -15,14 +15,9 @@ object Lists extends App:
       case Cons(h, t) => h + sum(t)
       case _ => 0
 
-    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil() => Nil()
+    def map[A, B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(x  => Cons(mapper(x), Nil()))
 
-    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
-      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
-      case Cons(_, t) => filter(t)(pred)
-      case Nil() => Nil()
+    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = flatMap(l1)( x => if (pred(x)) Cons(x, Nil()) else Nil())
 
     @tailrec
     def drop[A](l1: List[A], n: Int): List[A] = l1 match
